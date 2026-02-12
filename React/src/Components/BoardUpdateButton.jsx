a359f307b282c66f44629cea6a98e8e253c9d6b2
신재헌 <jaeheonshin@sinjaeheon-ui-MacBookAir.local>
2026-02-12 20:23:22 +0900

    Asd

---
+++ React/src/Components/BoardUpdateButton.jsx
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react'; 
+import api from '../api/api';
+
+
+
+const BoardUpdateButton = () => {
+
+
+    
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+
+export default BoardUpdateButton; 
